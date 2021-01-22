59912f5162e31bd7e1823a390787b49c09ca569c
aavashneupane <aavashneupane4@gmail.com>
2021-01-22 16:16:18 +0545

    linked login, register and dashboard activity

--- app/src/main/java/com/aavash/jobfinder/LoginActivity.kt
+++ app/src/main/java/com/aavash/jobfinder/LoginActivity.kt
@@ -1,11 +1,48 @@
 package com.aavash.jobfinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(),View.OnClickListener{
+
+    private lateinit var atvEmailLog:Button
+    private lateinit var atvPasswordLog:Button
+    private lateinit var btnSignIn:Button
+    private lateinit var btnSignUp:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+//        atvEmailLog=findViewById(R.id.atvEmailLog)
+  //      atvPasswordLog=findViewById(R.id.atvPasswordLog)
+
+        btnSignIn=findViewById(R.id.btnSignIn)
+        btnSignUp=findViewById(R.id.btnSignUp)
+
+        btnSignIn.setOnClickListener(this)
+        btnSignUp.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+
+        when(v?.id){
+            R.id.btnSignIn->{
+                val intent=Intent(this,homeScreen::class.java)
+                startActivity(intent)
+            }
+            R.id.btnSignUp->{
+                val intent=Intent(this,registration::class.java)
+                startActivity(intent)
+            }
+
+        }
+
     }
 }
--- app/src/main/java/com/aavash/jobfinder/registration.kt
+++ app/src/main/java/com/aavash/jobfinder/registration.kt
@@ -1,11 +1,37 @@
 package com.aavash.jobfinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class registration : AppCompatActivity() {
+class registration : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btnSignUp:Button
+    private lateinit var btnSignIn:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        btnSignIn=findViewById(R.id.btnSignIn)
+        btnSignUp=findViewById(R.id.btnSignUp)
+
+
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btnSignIn->{
+                val intent= Intent(this,LoginActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.btnSignUp->{
+                val intent= Intent(this,LoginActivity::class.java)
+                startActivity(intent)
+            }
+
+        }
     }
 }
