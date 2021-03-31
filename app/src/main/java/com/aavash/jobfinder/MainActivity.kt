9d4a7b3d657f1c8bba118c28915641a993a67a73
aavashneupane <aavashneupane4@gmail.com>
2021-03-31 13:51:21 +0545

    implemented fragments to navigate between three layout

--- app/src/main/java/com/aavash/jobfinder/MainActivity.kt
+++ app/src/main/java/com/aavash/jobfinder/MainActivity.kt
@@ -2,10 +2,35 @@ package com.aavash.jobfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.aavash.jobfinder.fragments.HomeFragment
+import com.aavash.jobfinder.fragments.JobsFragment
+import com.aavash.jobfinder.fragments.ProfileFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val HomeFragment=HomeFragment()
+        val JobsFragment=JobsFragment()
+        val ProfileFragment=ProfileFragment()
+
+        makeCurrentFragment(HomeFragment)
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.icHome->makeCurrentFragment(HomeFragment)
+                R.id.icJobs->makeCurrentFragment(JobsFragment)
+                R.id.icProfile->makeCurrentFragment(ProfileFragment)
+            }
+            true
+        }
+
     }
+    private fun makeCurrentFragment(fragment: Fragment)=
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fl_layout,fragment)
+                commit()
+            }
 }
--- app/src/main/java/com/aavash/jobfinder/api/ServiceBuilder.kt
+++ app/src/main/java/com/aavash/jobfinder/api/ServiceBuilder.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ServiceBuilder {
-    private const val BASE_URL = "http://192.168.31.12:91/"
+    private const val BASE_URL = "http://10.0.2.2:91/"
     var token :String? = null
 
 
