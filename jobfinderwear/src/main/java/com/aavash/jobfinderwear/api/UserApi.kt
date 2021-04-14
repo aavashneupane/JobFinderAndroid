71dd930f3171ffd98c561d43e7999bd4a2e7a894
aavashneupane <aavashneupane4@gmail.com>
2021-04-14 21:34:38 +0545

    added api for login and profile  for wearable

---
+++ jobfinderwear/src/main/java/com/aavash/jobfinderwear/api/UserApi.kt
@@ -0,0 +1,22 @@
+package com.aavash.jobfinderwear.api
+
+import retrofit2.Response
+import retrofit2.http.*
+
+interface UserApi {
+
+
+    //Login user
+    @FormUrlEncoded
+    @POST("user/login")
+    suspend fun checkUser(
+        @Field("email") email : String,
+        @Field("password") password : String
+    ): Response<LoginResponse>
+
+    //get users detail
+    @GET("profile2")
+    suspend fun getLoginUser(
+        @Header("Authorization") token: String
+    ): Response<UserResponse>
+}
